d0719a28f6ce02c3d2fcbf0a55c379476199deec
BriceWu <1003800829@qq.com>
2015-12-03 00:08:34 +0800

    保存Http数据

--- HongLingProject/HongLingProject.BLL/DealHttpData.cs
+++ HongLingProject/HongLingProject.BLL/DealHttpData.cs
@@ -4,27 +4,111 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using HongLingProject.Helper;
+using HongLingProject.DAL;
 
 namespace HongLingProject.BLL
 {
     public class DealHttpData
     {
-        public void GetHttpData()
-        {
-            string url = "https://www.my089.com/Loan/default.aspx?&oc=3&ou=1";
-            string IntersetRateMark= "<dd class=\"dd_2 mar_top_18\"><span class=\"number\">";
-            String[] spilt = new String[1] { "<dl class=\"LoanList\">" };
+        #region 私有全局变量
+        /// <summary>
+        /// 目标URL
+        /// </summary>
+        private static readonly string URL = "https://www.my089.com/Loan/default.aspx?&oc=3&ou=1";
+        /// <summary>
+        /// 分割字符串
+        /// </summary>
+        private static readonly String[] SpiltString = new String[1] { "<dl class=\"LoanList\">" };
+        /// <summary>
+        /// 利率标签
+        /// </summary>
+        private static readonly string IntersetRateMark = "<dd class=\"dd_2 mar_top_18\"><span class=\"number\">";
+        private static readonly string IntersetRateEndMark = "<b>%/年</b>";
+        /// <summary>
+        /// 期限，还款方式标签
+        /// </summary>
+        private static readonly string TimePaymentMark = "<dd class=\"dd_4 mar_top_18\"><span class=\"number\">";
+        private static readonly string TimePaymentEndMark = "</dd>";
 
-            string webData = HttpHelper.HttpGet(url, "");
-            string[] strArray = webData.Split(spilt, StringSplitOptions.RemoveEmptyEntries);
+        /// <summary>
+        /// 借款时间标签（第一个）
+        /// </summary>
+        private static readonly string LoadTimeMark = "<span class=\"lf\">";
+        private static readonly string LoadTimeEndMark = "</span>";
+        /// <summary>
+        /// 标类型标签
+        /// </summary>
+        private static readonly string MarkTypeMark = "<a href=\"/Loan/CateInfo.aspx\" target=\"_blank\">";
+        private static readonly string MarkTypeMark2= "class=\"";
+        private static readonly string MarkTypeEndMark = "\">";
+        private static Dictionary<string, string> MarkTypeDic;
+
+        #endregion 私有全局变量
+
+        InsertData insertData = new InsertData();
+        public DealHttpData()
+        {
+            MarkTypeDic = new Dictionary<string, string>();
+            MarkTypeDic.Add("SubL90", "净值标");
+            MarkTypeDic.Add("SubL110", "资产标");
+        }
+
+        /// <summary>
+        /// 保存Http数据
+        /// </summary>
+        public void SaveHttpData()
+        {
+            var lsRate = GetHttpData();
+            insertData.BathInsertInterestRate(lsRate);
+        }
+
+        public List<InterestRateModel> GetHttpData()
+        {
+            string webData = HttpHelper.HttpGet(URL, "");
+            string[] strArray = webData.Split(SpiltString, StringSplitOptions.RemoveEmptyEntries);
             var lsRate = new List<InterestRateModel>();
+
             for (int i = 1; i < 11; i++)
             {
                 var rate = new InterestRateModel();
-                int start = strArray[i].LastIndexOf(IntersetRateMark);
-                string str1 = strArray[i].Substring(start);
-                rate.InterestRate=decimal.Parse(str1.Substring(IntersetRateMark.Length, str1.IndexOf("<b>%/年</b>") - IntersetRateMark.Length));
+                //利率
+                string interestRate = strArray[i].Substring(strArray[i].IndexOf(IntersetRateMark) + IntersetRateMark.Length);
+                interestRate = interestRate.Substring(0, interestRate.IndexOf(IntersetRateEndMark));
+                //借款时间
+                string loadTime = strArray[i].Substring(strArray[i].IndexOf(LoadTimeMark)+LoadTimeMark.Length);
+                loadTime = loadTime.Substring(0, loadTime.IndexOf(LoadTimeEndMark));
+                //标类型
+                string markType = strArray[i].Substring(strArray[i].IndexOf(MarkTypeMark));
+                markType = markType.Substring(markType.IndexOf(MarkTypeMark2)+MarkTypeMark2.Length);
+                markType = markType.Substring(0, markType.IndexOf(MarkTypeEndMark));
+                //期限还款方式
+                string timePayment = strArray[i].Substring(strArray[i].IndexOf(TimePaymentMark) + TimePaymentMark.Length);
+                timePayment = timePayment.Substring(0, timePayment.IndexOf(TimePaymentEndMark));
+                timePayment = timePayment.Replace(" ", "");
+
+                int spanIndex = timePayment.IndexOf("</span>");
+                //期限
+                int timeLimint =int.Parse(timePayment.Substring(0,spanIndex));
+                timePayment = timePayment.Substring(spanIndex + "</span>".Length);
+                //3</span>个月/按月分期
+                if (timePayment.Substring(0, timePayment.IndexOf("/")).Contains("月"))
+                {
+                    //一个月按30天算
+                    timeLimint *= 30;
+                }
+                //还款方式
+                string paymentMethod = timePayment.Substring(timePayment.IndexOf("/") + "/".Length);
+
+                rate.InterestRate = decimal.Parse(interestRate);
+                rate.MarkTypeName = MarkTypeDic[markType];
+                rate.PaymentMethod = paymentMethod;
+                rate.LoadTime = DateTime.Parse(loadTime);
+                rate.TimeLimit = timeLimint;
+
+                lsRate.Add(rate);
             }
+
+            return lsRate;
         }
     }
 }
--- HongLingProject/HongLingTest/UnitTest1.cs
+++ HongLingProject/HongLingTest/UnitTest1.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using HongLingProject.Helper;
 using System.Collections.Generic;
+using HongLingProject.BLL;
 
 namespace HongLingTest
 {
@@ -11,17 +12,8 @@ namespace HongLingTest
         [TestMethod]
         public void TestMethod1()
         {
-           string str= HttpHelper.HttpGet("https://www.my089.com/Loan/default.aspx?&oc=3&ou=1","");
-           String[] spilt =new String[1]{ "<dl class=\"LoanList\">"};
-           string[] strArray = str.Split(spilt, StringSplitOptions.RemoveEmptyEntries);
-            List<double> lsd = new List<double>();
-           for(int i=1;i<11;i++)
-           {
-                string abc = "<dd class=\"dd_2 mar_top_18\"><span class=\"number\">";
-                int start= strArray[i].LastIndexOf(abc);
-                string str1=strArray[i].Substring(start);
-                lsd.Add(double.Parse(str1.Substring(abc.Length, str1.IndexOf("<b>%/年</b>")-abc.Length)));
-           }
+            DealHttpData deal = new DealHttpData();
+            deal.GetHttpData();
             Assert.Fail();
         }
     }
