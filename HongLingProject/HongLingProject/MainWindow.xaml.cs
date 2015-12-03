95f793817cb58dd366e8c9602fcfab6fc21e2306
BriceWu <1003800829@qq.com>
2015-12-03 21:46:07 +0800

    删减无关代码

--- HongLingProject/HongLingProject/MainWindow.xaml.cs
+++ HongLingProject/HongLingProject/MainWindow.xaml.cs
@@ -39,9 +39,6 @@ namespace HongLingProject
         public MainWindow()
         {
             InitializeComponent();
-            MarkBind();
-            PaymentMethodBind();
-            TimeBind();
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
@@ -70,42 +67,6 @@ namespace HongLingProject
             i++;
         }
 
-        /// <summary>
-        /// 标类型
-        /// </summary>
-        public void MarkBind()
-        {
-            var lsComb= dealData.DealMarkType();
-            Bind(Mark_ComboBox, lsComb);
-        }
-
-        /// <summary>
-        /// 还款方式
-        /// </summary>
-        /// <param name="dealData"></param>
-        public void PaymentMethodBind()
-        {
-            var lsComb = dealData.DealPaymentMethod();
-            Bind(Repayment_ComboBox, lsComb);
-        }
-
-        public void TimeBind()
-        {
-            string time = DateTime.Now.ToString("H-m-s");
-            string[] strTime = time.Split('-');
-            Hour_Text.Text = strTime[0];
-            Minute_Text.Text = strTime[1];
-            Second_Text.Text = strTime[2];
-        }
-
-        public void Bind(ComboBox combBox,List<ComboBoxModel> lsComb)
-        {
-            combBox.ItemsSource = lsComb;
-            combBox.DisplayMemberPath = "DisplayName";
-            combBox.SelectedValuePath = "ID";
-            combBox.SelectedValue = lsComb.Where(p => p.IsDefault).Select(p => p.ID).First();
-        }
-
         private void Insert_Button_Click(object sender, RoutedEventArgs e)
         {
             int AutoBidNo = int.Parse(AutomaticBid_Text.Text);
@@ -132,35 +93,6 @@ namespace HongLingProject
         {
             InterestRateMenu.Visibility = Visibility.Hidden;
             AutoBidMenu.Visibility = Visibility.Visible;
-        }
-
-        private void Save_Button_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void Import_Button_Click(object sender, RoutedEventArgs e)
-        {
-            string errorMsg = null;
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.Filter = "Excel文件(*.xls;*.xlsx)|*.xls;*.xlsx";
-            //设置默认打开目录
-            openFileDialog.InitialDirectory= dealData.GetPersonalAction("ImportInterestRate");
-            openFileDialog.Multiselect = false;
-
-            if (openFileDialog.ShowDialog()==System.Windows.Forms.DialogResult.OK)
-            {
-                string directory = openFileDialog.FileName.Substring(0, openFileDialog.FileName.LastIndexOf('\\'));
-                dealData.SetPersonalAction("ImportInterestRate",directory);
-                if(dealData.ReadInterestRate(openFileDialog.FileName, out errorMsg))
-                {
-                    MessageBox.Show("导入成功");
-                }
-                else
-                {
-                    MessageBox.Show("导入失败");
-                }
-            }
         }
     }
 }
